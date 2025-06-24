5615039deca44dfaabe64e0a59f0f5092d07d782
KostyaTeacher <sounddummies5@gmail.com>
2025-06-24 21:18:02 +0300

    Lesons 13

---
+++ 36.py
@@ -0,0 +1,11 @@
+cyrillic_to_latin = { 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ye',
+                      'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'yi', 'й': 'y', 'к': 'k', 'л': 'l',
+                      'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+                      'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ь': '', 'ю': 'iu', 'я': 'ia', ' ': ' ' }
+
+word = input("Введіть текст який хочете перекласти: ")
+
+for character in word:
+    word = word.replace(character,cyrillic_to_latin.get(character))
+
+print(word)
